2738805941b48d0721fa697eb934f09369ee40e2
coding-to-music <connors.tom@gmail.com>
2021-09-08 03:17:45 +0000

    build

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,15 +1,96 @@
-resource "digitalocean_droplet" "vault" {
-count              = var.instance_count
-image              = var.do_snapshot_id
-name               = var.do_name
-region             = var.do_region
-size               = var.do_size
-monitoring         = var.do_monitoring
-private_networking = var.do_private_networking
-ssh_keys = [
-  var.ssh_fingerprint
-]
+resource "digitalocean_droplet" "vault-01" {
+  name                = "vault-01"
+  # count               = var.instance_count
+  image               = var.do_snapshot_id
+  region              = var.do_region
+  size                = var.do_size
+  monitoring          = var.do_monitoring
+  private_networking  = var.do_private_networking
+  ssh_keys = [
+    var.ssh_fingerprint
+  ]
 }
+
+# create various droplets
+resource "digitalocean_droplet" "multi-01" {
+  name                = "multi-01"
+  count               = var.instance_count
+  image               = var.do_snapshot_id
+  region              = var.do_region
+  size                = var.do_size
+  monitoring          = var.do_monitoring
+  private_networking  = var.do_private_networking
+  ssh_keys = [
+    var.ssh_fingerprint
+  ]
+}
+
+resource "digitalocean_droplet" "grafana-01" {
+  name                = "grafana-01"
+  count               = var.instance_count
+  image               = var.do_snapshot_id
+  region              = var.do_region
+  size                = var.do_size
+  monitoring          = var.do_monitoring
+  private_networking  = var.do_private_networking
+  ssh_keys = [
+    var.ssh_fingerprint
+  ]
+}
+
+resource "digitalocean_droplet" "redis-01" {
+  name                = "redis-01"
+  count               = var.instance_count
+  image               = var.do_snapshot_id
+  region              = var.do_region
+  size                = var.do_size
+  monitoring          = var.do_monitoring
+  private_networking  = var.do_private_networking
+  ssh_keys = [
+    var.ssh_fingerprint
+  ]
+}
+
+resource "digitalocean_droplet" "influxdb-01" {
+  name                = "influxdb-01"
+  count               = var.instance_count
+  image               = var.do_snapshot_id
+  region              = var.do_region
+  size                = var.do_size
+  monitoring          = var.do_monitoring
+  private_networking  = var.do_private_networking
+  ssh_keys = [
+    var.ssh_fingerprint
+  ]
+}
+
+resource "digitalocean_droplet" "postgres-01" {
+  name                = "postgres-01"
+  count               = var.instance_count
+  image               = var.do_snapshot_id
+  region              = var.do_region
+  size                = var.do_size
+  monitoring          = var.do_monitoring
+  private_networking  = var.do_private_networking
+  ssh_keys = [
+    var.ssh_fingerprint
+  ]
+}
+
+resource "digitalocean_droplet" "mongodb-01" {
+  name                = "mongodb-01"
+  count               = var.instance_count
+  image               = var.do_snapshot_id
+  region              = var.do_region
+  size                = var.do_size
+  monitoring          = var.do_monitoring
+  private_networking  = var.do_private_networking
+  ssh_keys = [
+    var.ssh_fingerprint
+  ]
+}
+
+
 
 terraform {
   required_providers {
@@ -20,6 +101,48 @@ terraform {
   required_version = ">= 0.13"
 }
 
+# create an ansible inventory file
+resource "null_resource" "ansible-provision" {
+  depends_on = [
+    digitalocean_droplet.vault-01,
+    digitalocean_droplet.grafana-01,
+    digitalocean_droplet.mongodb-01,
+    digitalocean_droplet.postgres-01,
+    digitalocean_droplet.multi-01,
+    digitalocean_droplet.redis-01,
+    digitalocean_droplet.influxdb-01,
+  ]
+
+  provisioner "local-exec" {
+    command = "echo '${digitalocean_droplet.vault-01.name} ansible_host=${digitalocean_droplet.vault-01.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${digitalocean_droplet.grafana-01.name} ansible_host=${digitalocean_droplet.grafana-01.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${digitalocean_droplet.mongodb-01.name} ansible_host=${digitalocean_droplet.mongodb-01.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${digitalocean_droplet.postgres-01.name} ansible_host=${digitalocean_droplet.postgres-01.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${digitalocean_droplet.redis-01.name} ansible_host=${digitalocean_droplet.redis-01.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${digitalocean_droplet.multi-01.name} ansible_host=${digitalocean_droplet.multi-01.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${digitalocean_droplet.influxdb-02.name} ansible_host=${digitalocean_droplet.influxdb-02.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' >> inventory"
+  }
+}
+
+
 output "instance_ip_addr" {
 value = {
   for instance in digitalocean_droplet.vault:
@@ -27,3 +150,4 @@ value = {
 }
 description = "The IP addresses of the deployed instances, paired with their IDs."
 }
+
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -8,141 +8,93 @@
 #   token = var.do_token
 # }
 
-# create various droplets
-resource "digitalocean_droplet" "multi-01" {
-  name     = "multi-01"
-  image    = "ubuntu-20-04-x64"
-  region   = "nyc3"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [var.ssh_fingerprint]
-}
-
-resource "digitalocean_droplet" "grafana-01" {
-  name     = "grafana-01"
-  image    = "ubuntu-20-04-x64"
-  region   = "nyc3"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [var.ssh_fingerprint]
-}
-
-resource "digitalocean_droplet" "redis-01" {
-  name     = "redis-01"
-  image    = "ubuntu-20-04-x64"
-  region   = "nyc3"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [var.ssh_fingerprint]
-}
-
-resource "digitalocean_droplet" "influxdb-01" {
-  name     = "influxdb-01"
-  image    = "ubuntu-20-04-x64"
-  region   = "nyc3"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [var.ssh_fingerprint]
-}
-
-resource "digitalocean_droplet" "postgres-01" {
-  image    = "ubuntu-20-04-x64"
-  name     = "postgres-01"
-  region   = "nyc3"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [var.ssh_fingerprint]
-}
-
-resource "digitalocean_droplet" "mongodb-01" {
-  name     = "mongodb-01"
-  image    = "ubuntu-20-04-x64"
-  region   = "nyc3"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [var.ssh_fingerprint]
-}
 
 # create two demo droplets
-resource "digitalocean_droplet" "demo-01" {
-  name     = "demo-01"
-  image    = "ubuntu-20-04-x64"
-  region   = "nyc3"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [var.ssh_fingerprint]
-}
+# resource "digitalocean_droplet" "demo-01" {
+#   name     = "demo-01"
+#   image    = "ubuntu-20-04-x64"
+#   region   = "nyc3"
+#   size     = "s-1vcpu-1gb"
+#   ssh_keys = [var.ssh_fingerprint]
+# }
 
-resource "digitalocean_droplet" "demo-02" {
-  name     = "demo-02"
-  image    = "ubuntu-20-04-x64"
-  region   = "nyc3"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [var.ssh_fingerprint]
-}
+# resource "digitalocean_droplet" "demo-02" {
+#   name     = "demo-02"
+#   image    = "ubuntu-20-04-x64"
+#   region   = "nyc3"
+#   size     = "s-1vcpu-1gb"
+#   ssh_keys = [var.ssh_fingerprint]
+# }
 
 # create a loadbalancer that points to the two droplets
-resource "digitalocean_loadbalancer" "demo-lb" {
-  name   = "demo-lb"
-  region = "nyc3"
+# resource "digitalocean_loadbalancer" "demo-lb" {
+#   name   = "demo-lb"
+#   region = "nyc3"
 
-  droplet_ids = [
-    digitalocean_droplet.demo-01.id,
-    digitalocean_droplet.demo-02.id,
-  ]
+#   droplet_ids = [
+#     digitalocean_droplet.demo-01.id,
+#     digitalocean_droplet.demo-02.id,
+#   ]
 
-  forwarding_rule {
-    entry_port     = 80
-    entry_protocol = "http"
+#   forwarding_rule {
+#     entry_port     = 80
+#     entry_protocol = "http"
 
-    target_port     = 80
-    target_protocol = "http"
-  }
-}
+#     target_port     = 80
+#     target_protocol = "http"
+#   }
+# }
 
-# create a firewall that only accepts port 80 traffic from the load balancer
-resource "digitalocean_firewall" "demo-firewall" {
-  name = "demo-firewall"
+# # create a firewall that only accepts port 80 traffic from the load balancer
+# resource "digitalocean_firewall" "demo-firewall" {
+#   name = "demo-firewall"
 
-  droplet_ids = [
-    digitalocean_droplet.demo-01.id,
-    digitalocean_droplet.demo-02.id,
-  ]
+#   droplet_ids = [
+#     digitalocean_droplet.demo-01.id,
+#     digitalocean_droplet.demo-02.id,
+#   ]
 
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "22"
-    source_addresses = ["0.0.0.0/0"]
-  }
-  inbound_rule {
-    protocol                  = "tcp"
-    port_range                = "80"
-    source_load_balancer_uids = [digitalocean_loadbalancer.demo-lb.id]
-  }
+#   inbound_rule {
+#     protocol         = "tcp"
+#     port_range       = "22"
+#     source_addresses = ["0.0.0.0/0"]
+#   }
+#   inbound_rule {
+#     protocol                  = "tcp"
+#     port_range                = "80"
+#     source_load_balancer_uids = [digitalocean_loadbalancer.demo-lb.id]
+#   }
 
-  outbound_rule {
-    protocol              = "tcp"
-    port_range            = "all"
-    destination_addresses = ["0.0.0.0/0"]
-  }
-  outbound_rule {
-    protocol              = "udp"
-    port_range            = "all"
-    destination_addresses = ["0.0.0.0/0"]
-  }
-}
+#   outbound_rule {
+#     protocol              = "tcp"
+#     port_range            = "all"
+#     destination_addresses = ["0.0.0.0/0"]
+#   }
+#   outbound_rule {
+#     protocol              = "udp"
+#     port_range            = "all"
+#     destination_addresses = ["0.0.0.0/0"]
+#   }
+# }
 
-# create an ansible inventory file
-resource "null_resource" "ansible-provision" {
-  depends_on = [
-    digitalocean_droplet.demo-01,
-    digitalocean_droplet.demo-02,
-  ]
+# # create an ansible inventory file
+# resource "null_resource" "ansible-provision" {
+#   depends_on = [
+#     digitalocean_droplet.demo-01,
+#     digitalocean_droplet.demo-02,
+#   ]
 
-  provisioner "local-exec" {
-    command = "echo '${digitalocean_droplet.demo-01.name} ansible_host=${digitalocean_droplet.demo-01.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' > inventory"
-  }
+#   provisioner "local-exec" {
+#     command = "echo '${digitalocean_droplet.demo-01.name} ansible_host=${digitalocean_droplet.demo-01.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' > inventory"
+#   }
 
-  provisioner "local-exec" {
-    command = "echo '${digitalocean_droplet.demo-02.name} ansible_host=${digitalocean_droplet.demo-02.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' >> inventory"
-  }
-}
+#   provisioner "local-exec" {
+#     command = "echo '${digitalocean_droplet.demo-02.name} ansible_host=${digitalocean_droplet.demo-02.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' >> inventory"
+#   }
+# }
 
-# output the load balancer ip
-output "ip" {
-  value = digitalocean_loadbalancer.demo-lb.ip
-}
+# # output the load balancer ip
+# output "ip" {
+#   value = digitalocean_loadbalancer.demo-lb.ip
+# }
 
