be73a66e729bf14e4221eaa170f78322e00d6783
coding-to-music <connors.tom@gmail.com>
2021-09-08 03:59:03 +0000

    build

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -14,7 +14,7 @@ resource "digitalocean_droplet" "vault-01" {
 # create various droplets
 resource "digitalocean_droplet" "multi-01" {
   name                = "multi-01"
-  count               = var.instance_count
+  # count               = var.instance_count
   image               = var.do_snapshot_id
   region              = var.do_region
   size                = var.do_size
@@ -27,7 +27,6 @@ resource "digitalocean_droplet" "multi-01" {
 
 resource "digitalocean_droplet" "grafana-01" {
   name                = "grafana-01"
-  count               = var.instance_count
   image               = var.do_snapshot_id
   region              = var.do_region
   size                = var.do_size
@@ -40,7 +39,7 @@ resource "digitalocean_droplet" "grafana-01" {
 
 resource "digitalocean_droplet" "redis-01" {
   name                = "redis-01"
-  count               = var.instance_count
+  # count               = var.instance_count
   image               = var.do_snapshot_id
   region              = var.do_region
   size                = var.do_size
@@ -53,7 +52,7 @@ resource "digitalocean_droplet" "redis-01" {
 
 resource "digitalocean_droplet" "influxdb-01" {
   name                = "influxdb-01"
-  count               = var.instance_count
+  # count               = var.instance_count
   image               = var.do_snapshot_id
   region              = var.do_region
   size                = var.do_size
@@ -66,7 +65,7 @@ resource "digitalocean_droplet" "influxdb-01" {
 
 resource "digitalocean_droplet" "postgres-01" {
   name                = "postgres-01"
-  count               = var.instance_count
+  # count               = var.instance_count
   image               = var.do_snapshot_id
   region              = var.do_region
   size                = var.do_size
@@ -79,7 +78,7 @@ resource "digitalocean_droplet" "postgres-01" {
 
 resource "digitalocean_droplet" "mongodb-01" {
   name                = "mongodb-01"
-  count               = var.instance_count
+  # count               = var.instance_count
   image               = var.do_snapshot_id
   region              = var.do_region
   size                = var.do_size
@@ -138,16 +137,16 @@ resource "null_resource" "ansible-provision" {
   }
 
   provisioner "local-exec" {
-    command = "echo '${digitalocean_droplet.influxdb-02.name} ansible_host=${digitalocean_droplet.influxdb-02.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' >> inventory"
+    command = "echo '${digitalocean_droplet.influxdb-01.name} ansible_host=${digitalocean_droplet.influxdb-01.ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' >> inventory"
   }
 }
 
 
-output "instance_ip_addr" {
-value = {
-  for instance in digitalocean_droplet.vault:
-  instance.id => instance.ipv4_address
-}
-description = "The IP addresses of the deployed instances, paired with their IDs."
-}
+# output "instance_ip_addr" {
+# value = {
+#   for instance in digitalocean_droplet[]:
+#   instance.id => instance.ipv4_address
+# }
+# description = "The IP addresses of the deployed instances, paired with their IDs."
+# }
 
